cdf620bc6af796f59cca86b8141e6aa56e0417ba
aidanMarroyo <aidanarroyostudent@gmail.com>
2024-11-29 00:41:32 -0500

    removed textreveal import error

--- sections/home/TextRevealDemo.tsx
+++
@@ -1,9 +0,0 @@
-import TextReveal from '@/components/ui/text-reveal';
-
-export function TextRevealDemo() {
-  return (
-    <div className='z-10 flex min-h-20 items-center justify-center rounded-lg'>
-      <TextReveal text='Why Choose Sanova Solutions?' />
-    </div>
-  );
-}
