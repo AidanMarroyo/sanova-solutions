bcc3355b023ca8ffb93c847645818b99b594d485
aidanMarroyo <aidanarroyostudent@gmail.com>
2024-12-04 12:37:34 -0500

    try again

--- app/services/[serviceId]/page.tsx
+++ app/services/[serviceId]/page.tsx
@@ -1,20 +1,36 @@
-// app/services/[serviceId]/page.tsx
 import Hero from './Hero';
 import { CTA } from './CTA';
 import { ServiceName, services } from '@/lib/constants';
 
+interface ServicePageProps {
+  params: { serviceId: string };
+}
+
 // Static params generation (synchronous)
 export function generateStaticParams() {
-  // Returning serviceIds as static parameters
   return Object.keys(services).map((serviceId) => ({
-    serviceId: serviceId as ServiceName, // Explicitly asserting the type here
+    params: { serviceId },
   }));
+}
+
+export async function generateMetadata({
+  params,
+}: ServicePageProps): Promise<{ title: string; description: string }> {
+  const service = services[params.serviceId as ServiceName];
+  return {
+    title: service.name,
+    description: service.description,
+  };
 }
 
 // Page Component
 const Page = ({ params }: { params: { serviceId: ServiceName } }) => {
   // Fetch the service based on the serviceId from the params
   const service = services[params.serviceId];
+
+  if (!service) {
+    return <div>Service not found</div>;
+  }
 
   return (
     <div>
