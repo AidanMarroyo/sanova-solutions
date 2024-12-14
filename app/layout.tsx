3f4e974e925895d74731858237ce2a000ae1104b
aidanMarroyo <aidanarroyostudent@gmail.com>
2024-12-13 23:09:28 -0500

    removed fonts in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,9 @@
 import { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import { ThemeProvider } from '../components/theme-provider';
 import Navbar from '../components/Navbar';
 import Footer from '@/components/Footer';
 import { Toaster } from '@/components/ui/toaster';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title:
@@ -34,26 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <head>
-        {/* Font Preload */}
-        <link
-          rel='preload'
-          href='/fonts/GeistVF.woff'
-          as='font'
-          type='font/woff'
-          crossOrigin='anonymous'
-        />
-        <link
-          rel='preload'
-          href='/fonts/GeistMonoVF.woff'
-          as='font'
-          type='font/woff'
-          crossOrigin='anonymous'
-        />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <div className='fixed top-0 left-0 -z-10 w-full h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]' />
         <ThemeProvider
           attribute='class'
